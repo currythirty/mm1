0748ab9b8c482c2fdd40589a29010f2c05076325
currythirty <77310268+currythirty@users.noreply.github.com>
2021-02-01 02:59:10 +0800

    Update CompanyController.java

--- src/main/java/com/itheima/mm/controller/CompanyController.java
+++ src/main/java/com/itheima/mm/controller/CompanyController.java
@@ -30,6 +30,7 @@ public class CompanyController {
             System.out.println("helloworld3");
             System.out.println("helloworld4");
             System.out.println("helloworld5");
+            System.out.println("helloworld5");
             //1.完成功能：查询所有企业
             List<Company> companyList = companyService.complexList();
             //2.处理结果
